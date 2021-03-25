888b1af91d0bed7b94dafa941df430c83e04aa0d
KatKava <katryn.kav97@gmail.com>
2021-03-25 21:29:53 +0000

    added url pathway to API

--- animal-crossing-painting-api/js/main.js
+++ animal-crossing-painting-api/js/main.js
@@ -3,7 +3,7 @@ document.querySelector("button").addEventListener("click", getPaintingName)
 async function getPaintingName() {
  const paintName = document.querySelector("input").value
  try { 
-  const res = await fetch(`http://localhost:8000/api/paintings/${paintName}`)
+  const res = await fetch(`https://anima-crossing-api.herokuapp.com/api/paintings/${paintName}`)
   const data = await res.json()
 
   document.querySelector("h2").innerText = data.realName
